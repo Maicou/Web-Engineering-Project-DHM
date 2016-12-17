600a2df4468cf30a0e2ece12274d473d99008716
Maicou <marco1169@hotmail.de>
2016-12-17 11:07:14 +0100

    MM headers now with real pictures of AL and H buildings

--- html/header.inc.php
+++ html/header.inc.php
@@ -5,6 +5,6 @@
  * and open the template in the editor.
  */
 ?>
-<a href="../public/RentalAdministration/houseone"><img class="IMGtransparency1" src="../picture/house-02.jpg" alt="Style 2" title="Style 2" /></a> 
-<a href="../public/RentalAdministration/housetwo"><img class="IMGtransparency1" src="../picture/exterior11.jpg" alt="Style 2" title="Style 2" /></a>
+<a href="../public/RentalAdministration/houseone"><img class="IMGtransparency1" src="../picture/IMG_2507.jpg" alt="Anton-Leo-Str. 6" title="Anton-Leo-Str. 6" /></a> 
+<a href="../public/RentalAdministration/housetwo"><img class="IMGtransparency1" src="../picture/IMG_2520.jpg" alt="Hauensteinerstr. 7" title="Hauensteinerstr. 7" /></a>
 <!--<a href="../public/RentalAdministration/housethree"><img class="IMGtransparency1" src="../picture/AHC - 16 - 5.2mill.jpg" alt="Style 2" title="Style 2" /></a>-->
